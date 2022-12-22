f3df9ba0cf2a1eb728bc01297f6c92e4dea358e6
Zeynep Sumeyye Gungor <gungorsumeyye3520@gmail.com>
2022-12-23 00:15:32 +0300

    23/12/2022

---
+++ src/test/java/Appium/Appium01.java
@@ -0,0 +1,26 @@
+package Appium;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+public class Appium01 {
+    @Test
+    public void test() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+      // capabilities.setCapability("platformName","Android");
+      // capabilities.setCapability(CapabilityType.PLATFORM_NAME,"Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "R68T5005LRM");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\zeynepsümeyye\\IdeaProjects\\mobilTest\\src\\Apps\\gestureTool.apk");
+        capabilities.setCapability("appPackage", "com.davemac327.gesture.tool");
+        capabilities.setCapability("appActivity", "com.davemac327.gesture.tool.GestureBuilderActivity");
+
+        AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+    }
+}
